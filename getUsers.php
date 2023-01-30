84c48b08e8a7fd402462dab0d819561e5ddd02c8
Iñaki García <igardiet@gmail.com>
2023-01-30 17:22:40 +0100

    miquel update

--- getUsers.php
+++ getUsers.php
@@ -10,11 +10,16 @@ $getUsers = "SELECT * FROM myGuests";
 $useDb = "USE myDB";
 $conn->query($useDb);
 
-$response = $conn->query($getUsers);
+$result = $conn->query($getUsers);
 
-echo "<pre>";
-var_dump($response);
-echo "</pre>";
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["email"]. " ". $row["password"] ."<br>";
+  }
+} else {
+  echo "0 results";
+}
 
 $conn->close();
 ?>
