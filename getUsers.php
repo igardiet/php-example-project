cb1e35dca9f644dd4d037d12f81b3ba13ab73b57
Iñaki García <igardiet@gmail.com>
2023-01-30 17:36:22 +0100

    update

--- getUsers.php
+++ getUsers.php
@@ -15,7 +15,7 @@ $result = $conn->query($getUsers);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["email"]. " ". $row["password"] ."<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["email"]. " ". $row["password"] .  "<button onclick='deleteUser(event)' userId=".$row['id'].">delete</button> <br>";
   }
 } else {
   echo "0 results";
--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-const userInput = document.querySelector('#user')
-const emailInput = document.querySelector('#email')
-const passInput = document.querySelector('#pass')
-const submit = document.querySelector('#submit')
+const userInput = document.querySelector("#user");
+const emailInput = document.querySelector("#email");
+const passInput = document.querySelector("#pass");
+const submit = document.querySelector("#submit");
 
-submit.addEventListener('click', register)
+submit.addEventListener("click", register);
 
-function register(){
-    let name = userInput.value
-    let email = emailInput.value
-    let pass = passInput.value
+function register() {
+  let name = userInput.value;
+  let email = emailInput.value;
+  let pass = passInput.value;
 
-    // GET
-    // fetch(`createUser.php?name=${name}&email=${email}&pass=${pass}`)
-    // .then((res)=>res.json())
-    // .then((data)=> console.log(data))
+  let data = new FormData();
+  data.append("name", name);
+  data.append("email", email);
+  data.append("pass", pass);
 
-
-    // POST
-    let data = new FormData()
-    data.append("name", name)
-    data.append("email", email)
-    data.append("pass", pass)
-
-    fetch("createUser.php", {
+  fetch("createUser.php", {
     method: "POST",
-    body: data
-    }).then((res)=>res.json())
-    .then((data)=> console.log(data))
+    body: data,
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function deleteUser(event) {
+  const userId = event.target.getAttribute("userId");
+
+   fetch(`deleteUser.php?userId=${userId}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
